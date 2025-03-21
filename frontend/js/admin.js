4488fa061d9f17630cafbc2d860dbce9a0607770
Steven <falcontello76@gmail.com>
2025-03-21 01:53:18 -0500

    Arreglos

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -60,7 +60,7 @@ function abrirCurso(cursoId) {
 // Función para cerrar sesión
 function logout() {
     localStorage.clear();
-    window.location.href = "login.html";
+    window.location.href = "index.html";
 }
 
 document.getElementById("logout-btn").addEventListener("click", logout);
