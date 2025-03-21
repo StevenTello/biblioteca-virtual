1b0a74506fb2c2781874eba6e243550a68d4d7a3
Steven <falcontello76@gmail.com>
2025-03-21 01:33:44 -0500

    modules

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,7 @@
+console.log("Variables de entorno:", import.meta.env); // Ver todas las variables disponibles
+
 const API_URL = import.meta.env.VITE_API_URL || "https://biblioteca-virtual-production-377f.up.railway.app";
+console.log("API_URL:", API_URL); // Ver si la variable se carga correctamente
+
 export default API_URL;
+
