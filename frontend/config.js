bb0a9184040cc25b869bbd08713b922b09844255
Steven <falcontello76@gmail.com>
2025-03-21 00:18:04 -0500

    Cambio de rutas fetch para api url

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
-const API_URL = "https://biblioteca-virtual-production-377f.up.railway.app";
+const API_URL = import.meta.env.VITE_API_URL || "https://biblioteca-virtual-production-377f.up.railway.app";
 export default API_URL;
